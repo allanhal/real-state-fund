1587a1d8fd41efb5851ab04d1ba9e50f663815a7
Allan <allan.hal@gmail.com>
2018-09-25 00:00:24 -0300

    Added another API (FundsExplorer)

--- routes/fiis.js
+++ routes/fiis.js
@@ -6,7 +6,15 @@ var express = require('express');
 var router = express.Router();
 
 const URL = 'https://fiis.com.br/indicadores-estendido/'
+const URL_FUNDS_EXPLORER = 'http://www.fundsexplorer.com.br/funds'
 const prefferedFiis = require(path.join(__dirname, '../prefferedFiis'))
+
+const CACHE_IN_MINUTES = 20
+
+let cachedTablesAsJson;
+let tablesAsJson_expires_on = 0
+let cachedTablesAsJsonFromFundsExplorer;
+let tablesAsJsonFromFundsExplorer_expires_on = 0
 
 router.get('/my', function (req, res, next) {
   res.json(prefferedFiis)
@@ -27,41 +35,84 @@ router.get('/myComplete', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-  tabletojson.convertUrl(URL, {
-    stripHtmlFromCells: false
-  }, (tablesAsJson) => {
-    let fiis = tablesAsJson[0]
-    fiis = parseFiisJson(fiis)
+  getAllFiis().then(fiis => {
     res.json(fiis)
-  });
+  })
 });
 
 router.get('/:cod', function (req, res, next) {
   let cod = req.params.cod
-  tabletojson.convertUrl(URL, {
-    stripHtmlFromCells: false
-  }, (tablesAsJson) => {
-    let fiis = tablesAsJson[0]
-    fiis = parseFiisJson(fiis)
+  getAllFiis().then(fiis => {
     let fii = fiis.find(fii => fii.cod.toLowerCase() == cod.toLowerCase())
     if (fii) {
       res.json(fii)
     } else {
       res.status(400).send('Fii not found.')
     }
-  });
+    res.json(fiis)
+  })
 });
 
 function getAllFiis() {
   return new Promise((resolve, reject) => {
-    tabletojson.convertUrl(URL, {
-      stripHtmlFromCells: false
-    }, (tablesAsJson) => {
-      let fiis = tablesAsJson[0]
-      fiis = parseFiisJson(fiis)
+    getFiisFromApi().then(fiis => {
+      getFundsExplorer().then(fiisFromFundsExplorer => {
+        fiis.forEach(fii => {
+          fiiFromFundsExplorer = fiisFromFundsExplorer.find(fiiFromFundsExplorer => fiiFromFundsExplorer.cod == fii.cod)
+          if (fiiFromFundsExplorer) {
+            fii.price = fiiFromFundsExplorer.price
+          } else {
+            fii.price = undefined
+          }
+        });
+      })
       resolve(fiis)
     });
   })
+}
+
+function getFiisFromApi() {
+  return new Promise((resolve, reject) => {
+    if (new Date() > tablesAsJson_expires_on) {
+      tablesAsJson_expires_on = new Date(new Date().getTime() + (CACHE_IN_MINUTES * 60 * 1000))
+      tabletojson.convertUrl(URL, {
+        stripHtmlFromCells: false
+      }, (tablesAsJson) => {
+        let fiis = tablesAsJson[0]
+        cachedTablesAsJson = parseFiisJson(fiis)
+        resolve(cachedTablesAsJson)
+      });
+    } else {
+      resolve(cachedTablesAsJson)
+    }
+  })
+}
+
+function getFundsExplorer() {
+  return new Promise((resolve, reject) => {
+    if (new Date() > tablesAsJsonFromFundsExplorer_expires_on) {
+      tablesAsJsonFromFundsExplorer_expires_on = new Date(new Date().getTime() + (CACHE_IN_MINUTES * 60 * 1000))
+      tabletojson.convertUrl(URL_FUNDS_EXPLORER, tablesAsJsonFromFundsExplorer => {
+        let fiisFromFundsExplorer = tablesAsJsonFromFundsExplorer[0]
+        cachedTablesAsJsonFromFundsExplorer = parseFiisJsonFromFundsExplorer(fiisFromFundsExplorer)
+        resolve(cachedTablesAsJsonFromFundsExplorer)
+      });
+    } else {
+      resolve(cachedTablesAsJsonFromFundsExplorer)
+    }
+  })
+}
+
+function parseFiisJsonFromFundsExplorer(fiis) {
+  fiis = fiis.map(fii => parseFiiJsonFromFundsExplorer(fii));
+  return fiis
+}
+
+function parseFiiJsonFromFundsExplorer(fii) {
+  let toReturn = {}
+  toReturn.cod = fii['Ativo'].split('*')[0].trim()
+  toReturn.price = parseNumber(fii['Preço de Mercado'])
+  return toReturn
 }
 
 function parseFiisJson(fiis) {
